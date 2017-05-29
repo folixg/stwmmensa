6d4b04a14803d0b2df06e09c5a406a0c4291169f
Thomas Goldbrunner <thomas.goldbrunner@tum.de>
2017-05-29 10:44:15 +0200

    only keep main dishes, ignore sides

--- stwmmensa.go
+++ stwmmensa.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"strings"
 	"time"
 
 	"github.com/PuerkitoBio/goquery"
@@ -19,7 +20,7 @@ func main() {
 	// TODO handle weekends and holidays
 	now := time.Now()
 	var date string
-	if now.Hour() < 23 { // TODO change back to 15
+	if now.Hour() < 15 {
 		date = now.Format("2006-01-02")
 	} else {
 		date = now.Add(time.Duration(9) * time.Hour).Format("2006-01-02")
@@ -29,20 +30,26 @@ func main() {
 	// create url
 	baseURL := "http://www.studentenwerk-muenchen.de/mensa/speiseplan/"
 	menuURL := baseURL + "speiseplan_" + date + "_" + location + "_-de.html"
-	// TODO fetch html
+	// fetch html from Studentenwerk
 	doc, err := goquery.NewDocument(menuURL)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	dishes := make([]dish, 20) // TODO get actual size needed
-
+	// extract dishes
+	dishes := make([]dish, 0, 6)
+	var currentDish dish
 	doc.Find(".c-schedule__list-item").Each(func(i int, s *goquery.Selection) {
-		dishes[i].category = s.Find(".stwm-artname").Text()
-		if dishes[i].category == "" {
-			dishes[i].category = dishes[i-1].category
+		// get category (Tagesgericht, Aktionsessen, Beilage, ...)
+		currentDish.category = s.Find(".stwm-artname").Text()
+		// get dish description
+		currentDish.name = s.Find(".js-schedule-dish-description").Text()
+		// get rid of the "allergenkennzeichnungspflichtigen" ingredients
+		currentDish.name = strings.Split(currentDish.name, "[")[0]
+		if strings.HasPrefix(currentDish.category, "Tagesgericht") ||
+			strings.HasPrefix(currentDish.category, "Aktionsessen") ||
+			currentDish.category == "Self-Service" {
+			dishes = append(dishes, currentDish)
 		}
-		dishes[i].name = s.Find(".js-schedule-dish-description").Text()
 	})
 
 	for i := range dishes {
