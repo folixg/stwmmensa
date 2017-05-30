d98afa55807d30b7680ddcff0999cb78b9052073
Thomas Goldbrunner <thomas.goldbrunner@tum.de>
2017-05-30 10:11:35 +0200

    create html for LIS-infoscreen

--- stwmmensa.go
+++ stwmmensa.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"encoding/xml"
 	"fmt"
+	"html"
 	"log"
 	"os"
 	"strings"
@@ -11,17 +11,10 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
-// basic struct to store information about a dish
+// struct to store information about a dish
 type dish struct {
 	category string
 	name     string
-}
-
-// xml representation of a dish
-type xmlDish struct {
-	XMLName  xml.Name `xml:"dish"`
-	Name     string   `xml:"name,attr"`
-	Category string   `xml:"category,attr"`
 }
 
 // Wochentag contains the german names of the weekdays
@@ -82,44 +75,33 @@ func main() {
 		}
 	})
 
-	outFile, err := os.Create("menu.xml")
-
+	outFile, err := os.Create("menu.html")
 	if err != nil {
 		fmt.Println(err)
 	}
-	_, err = outFile.WriteString(xml.Header)
-	if err != nil {
-		log.Fatal(err)
-	}
 
-	var titleString string
-	// if we failed to fetch any dishes, assume mensa is closed
-	if len(dishes) == 0 {
-		titleString = "Leider hat die Mensa am " + Wochentag[now.Weekday()] + " den " +
-			now.Format("02.01.") + " geschlossen."
-	} else {
-		titleString = "Am " + Wochentag[now.Weekday()] + " den " +
-			now.Format("02.01.") + " empfiehlt Küchenchef Horst Waldner:"
-	}
-	_, err = outFile.WriteString("<menu title=\"" + titleString + "\">\n")
-	if err != nil {
-		log.Fatal(err)
-	}
+	titleString := "Mensa am " + Wochentag[now.Weekday()] + " den " +
+		now.Format("02.01.")
+	// set title
+	outFile.WriteString("<h1 class=\"mensa-title\">" +
+		html.EscapeString(titleString) + "</h1>\n")
+	// create box
+	outFile.WriteString("<div class=\"mensa-box\">\n")
+	// write dishes
 	if len(dishes) > 0 {
 		for i := range dishes {
-			d := &xmlDish{Name: dishes[i].name, Category: dishes[i].category}
-			output, err := xml.Marshal(d)
-			if err != nil {
-				log.Fatal(err)
-			}
-			outFile.Write(output)
-			outFile.WriteString("\n")
-
+			outFile.WriteString("<div class=\"mensa-item\">\n")
+			outFile.WriteString("<span class=\"mensa-name\">" +
+				html.EscapeString(dishes[i].category) + "</span>\n")
+			outFile.WriteString("<span class=\"mensa-value\">" +
+				html.EscapeString(dishes[i].name) + "</span>\n")
+			outFile.WriteString("</div>\n")
 		}
+	} else {
+		// if we failed to fetch any dishes, assume mensa is closed
+		outFile.WriteString("Leider geschlossen.")
 	}
-	_, err = outFile.WriteString("</menu>\n")
-	if err != nil {
-		log.Fatal(err)
-	}
+	// close box
+	outFile.WriteString("</div>\n")
 	outFile.Close()
 }
