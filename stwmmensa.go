ebfe28d55bab2371d8382cbe732a3f5eae2ee1e4
Thomas Goldbrunner <thomas.goldbrunner@tum.de>
2017-06-02 09:24:08 +0200

    fixed too long godoc description

--- stwmmensa.go
+++ stwmmensa.go
@@ -1,31 +1,30 @@
-/*
-stwmmensa fetches the canteen menu from the Studentenwerk Muenchen website
-
-stwmmensa accepts the following arguments:
-	-h --help: Print this usage info and quit
-	-o --output: set the path to the output file (mandatory)
-	-l --location: set mensa location id (default: 421)
-	-f --format: select format (default: xml)
-
-	** Location
-	Here is a list of valid location codes with the name of the corresponding mensa:
-	411 : Mensa Leopoldstraße
-	412 : Mensa Martinsried
-	421 : Mensa Arcisstraße
-	422 : Mensa Garching
-	423 : Mensa Weihenstephan
-	431 : Mensa Lothstraße
-	432 : Mensa Pasing
-
-	** Format
-	xml : a generic xml file is created
-	lis : generate html snippet for LIS-infoscreen
-
-	** Examples:
-	stwmmensa -l 411 -o /my/path/leopold.xml -f xml
-	stwmmensa --location=423 --output=weihenstephan.html --format=lis
-*/
+//stwmmensa fetches the canteen menu from the Studentenwerk Muenchen website
 package main
+
+/*stwmmensa accepts the following arguments:
+-h --help: Print this usage info and quit
+-o --output: set the path to the output file (mandatory)
+-l --location: set mensa location id (default: 421)
+-f --format: select format (default: xml)
+
+** Location
+Here is a list of valid location codes with the name of the corresponding mensa:
+411 : Mensa Leopoldstraße
+412 : Mensa Martinsried
+421 : Mensa Arcisstraße
+422 : Mensa Garching
+423 : Mensa Weihenstephan
+431 : Mensa Lothstraße
+432 : Mensa Pasing
+
+** Format
+xml : a generic xml file is created
+lis : generate html snippet for LIS-infoscreen
+
+** Examples:
+stwmmensa -l 411 -o /my/path/leopold.xml -f xml
+stwmmensa --location=423 --output=weihenstephan.html --format=lis
+*/
 
 import (
 	"fmt"
